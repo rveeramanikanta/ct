ae2f9f9a70c36bfdbfe13a75365468b132f28c60
rveeramanikanta <rveeramanikanta>
2017-08-22 17:13:45 +0530

    modification in circular queue

--- js/circular-queue-using-array.js
+++ js/circular-queue-using-array.js
@@ -54,12 +54,12 @@ function initIntroJS() {
 		case "queueInit":
 			$(".introjs-nextbutton").hide();
 				$(".introjs-tooltiptext").append("<ul></ul>");
-				var text = "<li>Let us define a <span class='ct-code-b-yellow'>QUEUE_MAX_SIZE</span> with " 
+				var text = "<li>Let us define jaksjdjfds a <span class='ct-code-b-yellow'>QUEUE_MAX_SIZE</span> with " 
 						+ "value <span class='ct-code-b-yellow'>10</span>.</li>" 
 						+ "<li>Let us declare an <span class='ct-code-b-yellow'>int</span>" 
 						+ " array variable <span class='ct-code-b-yellow'>queue</span>" 
 						+ " and <span class='ct-code-b-yellow'>int</span> " 
-						+ "variables are <y>front</y>, <y>rear1</y>.</li>";	
+						+ "variables are <y>front</y>, <y>rear</y>.</li>";	
 				
 			typing($(".introjs-tooltiptext ul"), text, function() {
 				$(".introjs-nextbutton").show();
